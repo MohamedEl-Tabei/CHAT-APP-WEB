0398741ced6871e8b7ab716a383624b94af12521
unknown <mohamedeltab33y@gmail.com>
2024-03-01 20:41:54 +0200

    delete users

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,12 @@
 import Components from "../base/components";
 import { useSelector } from "react-redux";
+import DeleteUsers from "../components/deleteUsers";
 
 const Home = () => {
   const user = useSelector((s) => s.user);
   return (
     <div>
+      <DeleteUsers/>
       <h1>{user.name}</h1>{" "}
       {user.name.length ? <Components.AuthHome /> : <Components.UnAuthHome />}
     </div>
